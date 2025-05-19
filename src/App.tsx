0b8b1145078c329f1ff3298833230cca83540af8
John Nunns <johndnunns@gmail.com>
2025-05-19 10:19:19 -0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <main className="flex gap-1">
+      <div className="bg-sidebar">test</div>
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-[20px] max-w-[1200px] mx-auto">
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
