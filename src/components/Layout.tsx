7ea65c577c5c1900cb7c421309b75442abb362c1
John Nunns <johndnunns@gmail.com>
2025-05-31 16:12:04 -0700

    styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = () => {
   return (
     <main className="flex gap-1">
       <Sidebar />
-      <div className="p-8 w-full max-w-[1200px] mx-auto pl-[70px]">
+      <div className="p-8 w-full max-w-[1200px] mx-auto pl-[70px] min-h-[100vh]">
         <Outlet />
       </div>
     </main>
